68140ebf53bf245acd37c41ad3f38119cb0800a0
iamyoungjin <nnna08@likelion.org>
2021-02-07 00:24:10 +0900

    18405.py

--- backjoon/DFS&BFS/18405.py
+++ backjoon/DFS&BFS/18405.py
@@ -1,2 +1,18 @@
 #https://www.acmicpc.net/problem/18405
 #경쟁적 전염
+#BFS
+
+from sys import stdin 
+from collections import deque
+
+move = [[-1,0],[1,0],[0,-1],[0,1]]
+N , K = map(int, stdin.readline().split())
+board = [list(map(int,stdin.readline().split())) for _ in range(N)]
+S, X, Y = map(int, stdin.readline().split())
+
+q = []
+for i in range(N):
+    for j in range(N):
+        if board[i][j]!= 0:
+            q.append([board[i][j],i,j,0])
+        
