5af117f097e8ce5ec0c471538c9ae749db661a17
iamyoungjin <nnna08@likelion.org>
2021-02-09 22:48:24 +0900

    :added comment

--- backjoon/11729.py
+++ backjoon/11729.py
@@ -1,14 +1,16 @@
 #hanoi
+'''
 
+'''
 n = int(input())
 
 def hanoi(n,from_,middle,to_):
     if n == 1:
         print(from_,to_)
     else:
-        hanoi(n-1,from_,to_,middle)
-        print(from_,to_)
-        hanoi(n-1,middle,from_,to_)
+        hanoi(n-1,from_,to_,middle) #1->2
+        print(from_,to_) # 1->3
+        hanoi(n-1,middle,from_,to_) #2->3
 
 sum =1
 for i in range(n-1):
