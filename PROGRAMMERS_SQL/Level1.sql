11da810445c294d8281be2f1633f1cbae9f00aed
iamyoungjin <nnna08@likelion.org>
2021-02-10 22:54:57 +0900

    :added SQL

--- PROGRAMMERS_SQL/Level1.sql
+++ PROGRAMMERS_SQL/Level1.sql
@@ -23,6 +23,32 @@ SELECT name, datetime from ANIMAL_INS order by ANIMAl_ID desc
 
 --아픈 동물 찾기
 '''
+-- 코드를 입력하세요
+
 https://programmers.co.kr/learn/courses/30/lessons/59036
 SELECT ANIMAL_ID, NAME from ANIMAL_INS where INTAKE_CONDITION = 'SICK' order by ANIMAL_ID
+'''
+
+--어린 동물 찾기
+'''
+https://programmers.co.kr/learn/courses/30/lessons/59037
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME  from ANIMAL_INS
+where INTAKE_CONDITION != 'Aged' order by ANIMAL_ID
+
+'''
+
+--동물의 아이디와 이름
+'''
+https://programmers.co.kr/learn/courses/30/lessons/59403
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS ORDER BY ANIMAL_ID
+'''
+
+--여러 기준으로 정렬하기
+'''
+https://programmers.co.kr/learn/courses/30/lessons/59404
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, DATETIME FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC
 '''
