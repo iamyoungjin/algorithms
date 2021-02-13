4b061e00c0eb64c79fa72dc61333ba777da9eb98
iamyoungjin <nnna08@likelion.org>
2021-02-14 00:05:43 +0900

    :added Level3 (2)

--- PROGRAMMERS_SQL/Level3.sql
+++ PROGRAMMERS_SQL/Level3.sql
@@ -6,3 +6,11 @@ LEFT JOIN ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID
 WHERE B.ANIMAL_ID IS NULL ORDER BY A.DATETIME limit 3
 '''
 
+--오랜 기간 보호한 동물(2)
+'''
+https://programmers.co.kr/learn/courses/30/lessons/59411
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID , A.NAME FROM ANIMAL_OUTS as A
+JOIN ANIMAL_INS as B ON A.ANIMAL_ID = B.ANIMAL_ID 
+ORDER BY (A.DATETIME - B.DATETIME) desc LIMIT 2;
+'''
