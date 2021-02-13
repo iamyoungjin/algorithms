de58b6630d1df9b59aa463dbd4d19b1a8985872d
iamyoungjin <nnna08@likelion.org>
2021-02-13 23:17:41 +0900

    :added Level3 (1)

--- PROGRAMMERS_SQL/Level3.sql
+++ PROGRAMMERS_SQL/Level3.sql
@@ -1,4 +1,8 @@
 --오랜 기간 보호한 동물(1)
 '''
 https://programmers.co.kr/learn/courses/30/lessons/59044
+SELECT A.NAME, A.DATETIME FROM ANIMAL_INS A
+LEFT JOIN ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE B.ANIMAL_ID IS NULL ORDER BY A.DATETIME limit 3
 '''
+
