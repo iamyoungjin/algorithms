f8b39186c34f038ccdff6e28f5603b2149c11ff0
iamyoungjin <nnna08@likelion.org>
2021-02-14 11:38:58 +0900

    :added Level3 (3)

--- PROGRAMMERS_SQL/Level3.sql
+++ PROGRAMMERS_SQL/Level3.sql
@@ -14,3 +14,16 @@ SELECT A.ANIMAL_ID , A.NAME FROM ANIMAL_OUTS as A
 JOIN ANIMAL_INS as B ON A.ANIMAL_ID = B.ANIMAL_ID 
 ORDER BY (A.DATETIME - B.DATETIME) desc LIMIT 2;
 '''
+
+
+--없어진 기록 찾기
+'''
+https://programmers.co.kr/learn/courses/30/lessons/59042
+SELECT A.ANIMAL_ID, A.NAME FROM ANIMAL_OUTS A
+LEFT OUTER JOIN ANIMAL_INS AS B ON (A.ANIMAL_ID = B.ANIMAL_ID)
+WHERE B.ANIMAL_ID IS NULL
+
+--or 
+SELECT animal_id , name from animal_outs where animal_id not in (select animal_id from animal_ins)
+
+'''
