4a1fb962d21e996980e09d7b1bbafa40d2b45e49
iamyoungjin <nnna08@likelion.org>
2021-02-17 22:57:12 +0900

    :added Level4 (2)

--- PROGRAMMERS_SQL/Level4.sql
+++ PROGRAMMERS_SQL/Level4.sql
@@ -6,3 +6,16 @@ SELECT A.CART_ID FROM (SELECT CART_ID FROM CART_PRODUCTS WHERE NAME = 'Milk') AS
 JOIN (SELECT CART_ID FROM CART_PRODUCTS WHERE NAME = 'Yogurt') AS B
 ON (A.CART_ID = B.CART_ID)ORDER BY A.CART_ID
 '''
+
+--보호소에서 중성화한 동물
+
+'''
+https://programmers.co.kr/learn/courses/30/lessons/59045
+-- 코드를 입력하세요
+SELECT A.ANIMAL_ID, A.ANIMAL_TYPE, A.NAME FROM ANIMAL_OUTS AS A
+JOIN ANIMAL_INS AS B on A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.SEX_UPON_OUTCOME IN ('Spayed Female', 'Neutered Male')
+AND B.SEX_UPON_INTAKE IN ('Intact Male', 'Intact Female')
+ORDER BY ANIMAL_ID;
+
+'''
