f67f1564d77fcdc3ed073fb15415700d16d468f9
iamyoungjin <nnna08@likelion.org>
2021-02-21 00:12:00 +0900

    2798

--- backjoon/2798.py
+++ backjoon/2798.py
@@ -2,3 +2,14 @@
 
 n,m = list(map(int,input().split(' ')))
 card =list(map(int(input().split(' '))))
+
+res = 0
+length = len(card)
+
+for i in range(0,length):
+    for j in range(i+1,length):
+        for k in range(j+1,length):
+            sum_val = card[i] + card[j] + card[k]
+            if sum_val <= m:
+                res = max(res, sum_val)
+print(res)
