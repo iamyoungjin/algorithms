3eaa644784c7facd7fe05261faf43ca0174ec1a5
iamyoungjin <nnna08@likelion.org>
2021-02-27 22:44:08 +0900

    4195

--- backjoon/4195.py
+++ backjoon/4195.py
@@ -1,16 +1,35 @@
-# Root node를 찾아주는 함수
+
+import sys
 def find(x):
-    if x == parent[x]:
+    if p[x]==x:
         return x
     else:
-        parent_x = find(parent[x])
-        parent[x] = parent_x
-        return parent[x]
-
-# y의 Root 노드가 x의 Root 노드와 같지 않으면 
-# y의 Root 노드가 x의 Root 노드의 자식이 되도록 하는 함수
+        y = find(p[x])
+        p[x] = y#시간을 줄이는 요소
+        return y
+ 
+ 
+ 
 def union(x,y):
-    x = find(x)
-    y = find(y)
-
+    x=find(x)
+    y=find(y)
     if x!=y:
+        p[y]=x#루트노드 합치기
+        cnt[x]=cnt[y]+cnt[x]#루트노드 수합치기
+
+test_case =int(input())
+for _ in range(test_case):
+    p = {}
+    cnt = {}
+    f=int(input())
+    for _ in range(f):
+        x,y= input().split()
+        if x not in p:#f1이 딕셔너리에 없다면
+            p[x]=x
+            cnt[x]=1
+        if y not in p:#f2이 딕셔너리에 없다면
+            p[y]=y
+            cnt[y]=1
+ 
+        union(x,y)
+        print(cnt[find(x)])
