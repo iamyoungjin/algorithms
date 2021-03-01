b24f1fb22a5b5b175d54955ce0e0ca014e7746c2
iamyoungjin <nnna08@likelion.org>
2021-03-01 20:37:20 +0900

    10814

--- backjoon/10814.py
+++ backjoon/10814.py
@@ -4,4 +4,9 @@ lst_user = []
 for _ in range(num_user):
     age_user, name_user = map(str,input().split(' '))
     age_user = int(age_user)
-    lst_user.append((age_user,num_user))
+    lst_user.append((age_user,name_user))
+
+lst_user = sorted(lst_user, key=lambda x: x[0])
+
+for i in lst_user:
+    print(i[0],i[1])
