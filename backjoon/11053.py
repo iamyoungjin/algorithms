1e5de2454146fa7664eeed9fd035807b55ab9f82
iamyoungjin <nnna08@likelion.org>
2021-05-13 21:15:44 +0900

    12015.py

--- backjoon/11053.py
+++ backjoon/11053.py
@@ -10,7 +10,7 @@ dp = [1 for i in range(x)]
 
 for i in range(x):
     for j in range(i):
-        if arr[i] > arr[j]:
+        if arr[i] > arr[j]: #현재 위치(i)보다 이전 원소(j)가 작은지 확인 (크거나 같으면 가장 긴 증가하는 부분 수열이 될 수 없음)
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
