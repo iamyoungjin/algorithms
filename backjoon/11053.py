9d5a50949e0a11e64e605a47fabd2b2f012e22a5
iamyoungjin <nnna08@likelion.org>
2021-05-13 21:06:10 +0900

    11053.py

---
+++ backjoon/11053.py
@@ -0,0 +1,17 @@
+#https://www.acmicpc.net/problem/11053
+
+
+#Dynamic Programming 풀이법
+x = int(input())
+
+arr = list(map(int, input().split()))
+
+dp = [1 for i in range(x)]
+
+for i in range(x):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
+
